c34d8645a4a02234c06ff1e3557e3777a7d72591
jaredh123 <jared.hanson12345@gmail.com>
2019-03-22 15:16:31 -0700

    Add date object

--- src/backEnd.js
+++ src/backEnd.js
@@ -1,42 +1,38 @@
 $(document).ready(function() {
-
-  // var health = $("#health").val();
-
   $("#run").submit(function(event) {
     event.preventDefault();
 
     var d1 = parseInt($("#day").val());
     var m1 = parseInt($("#month").val());
     var yr1 = parseInt($("#year").val());
+    var health = parseInt($("#health").val());
 
-    var d2 = 21;
-    var m2 = 3;
-    var yr2 = 2019;
+    var date = new Date();
 
     var month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-    var earthAge = function() {
-      var totalCalCount = function() {
-        var dayCount = month[m1 - 1] - d1 + d2;
-        var mCount;
-        var yrCount;
-        if ((month.length - m1 + m2 -1) >= 12) {
-          mCount = month.length - m1 + m2 - 13;
-          yrCount = yr2 - yr1;
-        }
-        else {
-          mCount = month.length - m1 + m2 - 1;
-          yrCount = yr2 - yr1 - 1;
-        }
-        if (d1 > month[m1 - 1] || m1 > (month.length) || d2 > month[m2 - 1] || m2 > (month.length)) {
-          return "That date doesn't exist!";
-        }
-        else {
-          return "You are " + yrCount + " years, " + mCount + " months, and " + dayCount + " days old on Earth";
-        }
+    var earthYears = function() {
+      var dayCount = month[m1 - 1] - d1 + date.getDate();
+      var mCount;
+      var yrCount;
+
+      if ((month.length - m1 + date.getMonth() -1) >= 12) {
+        mCount = month.length - m1 + date.getMonth() - 13;//Earth month count part 1/2
+        yrCount = date.getFullYear() - yr1;//Earth year count part 1/2
       }
-      return totalCalCount();
+      else {
+        mCount = month.length - m1 + date.getMonth() - 1;//Earth month count part 2/2
+        yrCount = date.getFullYear() - yr1 - 1;//Earth year part 2/2
+      }
+      if (d1 > month[m1 - 1] || m1 > (month.length) || date.getDate() > month[date.getMonth() - 1] || date.getMonth() > (month.length)) {
+        return "That date doesn't exist!";//False dates
+      }
+      else {
+        return yrCount + mCount + dayCount;//Earth full year count part 1/1
+      }
     }
-    $("#result").text(earthAge());
+
+
+    $("#result").text(date);
   });
 });
