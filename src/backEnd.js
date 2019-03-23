a6350ccd865f5e17fd22c7d4c6fd7ca20cb9e850
jaredh123 <jared.hanson12345@gmail.com>
2019-03-22 18:50:11 -0700

    Fix total earth year count with two decimal places

--- src/backEnd.js
+++ src/backEnd.js
@@ -6,31 +6,43 @@ $(document).ready(function() {
     var m1 = parseInt($("#month").val());
     var yr1 = parseInt($("#year").val());
     var health = parseInt($("#health").val());
+    var celestialBody = parseInt($("#celestialBody").val());
 
     var date = new Date();
-
     var month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
     var earthYears = function() {
       var dayCount = month[m1 - 1] - d1 + date.getDate();
-
-      if ((month.length - m1 + (date.getMonth() + 1) - 1) >= 12) {
-        mCount = month.length - m1 + (date.getMonth() + 1) - 13;//Earth month count part 1/2
-        yrCount = date.getFullYear() - yr1;//Earth year count part 1/2
-      }
-      else {
-        mCount = month.length - m1 + (date.getMonth() + 1) - 1;//Earth month count part 2/2
-        yrCount = date.getFullYear() - yr1 - 1;//Earth year part 2/2
+      var yrCount = date.getFullYear() - yr1 - 1;
+      var mDayCount = function() {
+        var count = 0;
+        for (var i = m1 + 1; i <= month.length; i++) {
+          if ((yr1 % 4 === 0) && (yr1 % 100 !== 0) || (yr1 % 400 === 0)) {
+            month[1] = 29;
+          }
+          else {
+            month[1] = 28;
+          }
+          count += month[i - 1];
+        }
+        for (var i = 0; i < date.getMonth(); i++) {
+          if ((date.getFullYear() % 4 === 0) && (date.getFullYear() % 100 !== 0) || (date.getFullYear() % 400 === 0)) {
+            month[1] = 29;
+          }
+          else {
+            month[1] = 28;
+          }
+          count += month[i];
+        }
+        return count;
       }
       if (d1 > month[m1 - 1] || m1 > (month.length) || date.getDate() > month[(date.getMonth() + 1) - 1] || (date.getMonth() + 1) > (month.length)) {
-        return "That date doesn't exist!";//False dates
+        return "That date doesn't exist!";
       }
       else {
-        return yrCount + mCount + dayCount;//Earth full year count part 1/1
+        return yrCount + parseFloat(((mDayCount() + dayCount) / 365).toFixed(2));
       }
     }
-
-
-    $("#result").text(date);
+    $("#result").text(earthYears());
   });
 });
